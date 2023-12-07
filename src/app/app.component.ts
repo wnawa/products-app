105046aa17a359ab9d74a520b4c2596b8f78df27
walaa nawar <walaa_nawar86@yahoo.com>
2023-12-07 21:05:41 +0100

    adding temple,databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent, FooterComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'products-app';
+  
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css',
+})
+export class ProductsComponent {
+  classPageTitle: string = 'text-center';
+  stylePageTitle = {
+    // 'color':'red',
+   
+    margin: 'auto'
+  };
+  mainThemeColor = 'Green';
+  isLogin=true;
+  pruductArr = [
+    {
+      id: 1,
+      Name: 'Glasses',
+      detail: 'Top Quality Sun glasses polarized',
+      imgPath: '../../assets/images/product-1.avif',
+      color:'Green',
+      Price:'200$',
+    },
+    {
+      id: 2,
+      Name: 'High Chair',
+      detail: 'High chair pin wood',
+      imgPath: '../../assets/images/product-2.avif',
+      color:'White',
+      Price:'200$',
+    },
+    {
+      id: 3,
+      Name: 'Nike Shoes',
+      detail: 'Sport trainer ',
+      imgPath: '../../assets/images/product-3.avif',
+      color:'Red',
+      Price:'200$',
+    },
+    {
+      id: 4,
+      Name: 'Saniteizers',
+      detail: '99,9 percent antibacterial ',
+      imgPath: '../../assets/images/product-4.avif',
+      color:'Blue',
+      Price:'200$',
+    },
+    {
+      id: 5,
+      Name: 'Toy',
+      detail: 'Car miniatures',
+      imgPath: '../../assets/images/product-5.avif',
+      color:'white',
+      Price:'200$',
+    },
+    {
+      id: 6,
+      Name: 'Apple smartwatch',
+      detail: 'RAM 128 GB.',
+      imgPath: '../../assets/images/product-6.avif',
+      color:'',
+      Price:'200$',
+    },
+
+    {
+      id: 7,
+      Name: 'Headphones',
+      detail: 'Dolbey sound system',
+      imgPath: '../../assets/images/product-7.avif',
+      color:'',
+      Price:'200$',
+    },
+  ];
+}
