12e11220a85dea6545ab09a7b67c3d2a3e312f3f
walaa nawar <walaa_nawar86@yahoo.com>
2023-12-18 11:41:53 +0100

    adding Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { CartPageComponent } from './cart-page/cart-page.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import {LoginComponent} from './Login/Login.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
@@ -12,4 +14,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterFormComponent },
   { path: 'login', component: LoginComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
