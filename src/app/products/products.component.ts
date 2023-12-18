8be06301e92cf183da91d68b250eebd05f70a9ea
walaa nawar <walaa_nawar86@yahoo.com>
2023-12-18 18:32:51 +0100

    adding Model ad services

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Product } from '../shared/models/Product';
+import { ProductServices } from '../services/products.services';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, FormsModule,RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  //start Pipe Lecture
   todayDAte = new Date();
   personWeight = 100.5;
-
   classPageTitle: string = 'text-center';
   stylePageTitle = {
     // 'color':'red',
@@ -21,63 +23,14 @@ export class ProductsComponent {
   };
   mainThemeColor = 'Green';
   isLogin = true;
-  pruductArr = [
-    {
-      id: 1,
-      Name: 'Glasses',
-      detail: 'Top Quality Sun glasses polarized',
-      imgPath: '../../assets/images/product-1.avif',
-      color: 'Green',
-      Price: '200$',
-    },
-    {
-      id: 2,
-      Name: 'High Chair',
-      detail: 'High chair pin wood',
-      imgPath: '../../assets/images/product-2.avif',
-      color: 'White',
-      Price: '200$',
-    },
-    {
-      id: 3,
-      Name: 'Nike Shoes',
-      detail: 'Sport trainer ',
-      imgPath: '../../assets/images/product-3.avif',
-      color: 'Red',
-      Price: '200$',
-    },
-    {
-      id: 4,
-      Name: 'Saniteizers',
-      detail: '99,9 percent antibacterial ',
-      imgPath: '../../assets/images/product-4.avif',
-      color: 'Blue',
-      Price: '200$',
-    },
-    {
-      id: 5,
-      Name: 'Toy',
-      detail: 'Car miniatures',
-      imgPath: '../../assets/images/product-5.avif',
-      color: 'white',
-      Price: '200$',
-    },
-    {
-      id: 6,
-      Name: 'Apple smartwatch',
-      detail: 'RAM 128 GB.',
-      imgPath: '../../assets/images/product-6.avif',
-      color: '',
-      Price: '200$',
-    },
+  //End Pipe Lecture
 
-    {
-      id: 7,
-      Name: 'Headphones',
-      detail: 'Dolbey sound system',
-      imgPath: '../../assets/images/product-7.avif',
-      color: '',
-      Price: '200$',
-    },
-  ];
+  //start services Model Lecture 14
+  pruductArr: Product[];
+  constructor(private productServices:ProductServices) {
+    //wrong syntax
+    // let productServices=new  ProductServices();
+
+    this.pruductArr = productServices.getAllProducts();
+  }
 }
---
+++ src/app/shared/models/Product.ts
@@ -0,0 +1,14 @@
+//Product Model
+export class Product {
+  id!: number;
+  Name!: string;
+  detail?: string;
+  imgPath?: string;
+  color?: string;
+  Price!: string;
+
+//   constructor(id, Name) {
+//     this.id = id;
+//     this.Name = Name;
+//   }
+}
